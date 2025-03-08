f80b5df15e7d1daed80ba7727c3a7d3cbb1ad1cb
AyushDeuja <ayushdeuja11@gmail.com>
2025-03-08 20:56:31 +0545

    header

---
+++ src/components/Head.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
+
+const Head = () => {
+  const dispatch = useDispatch();
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+
+  return (
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg ">
+      <div className="flex col-span-1">
+        <img
+          onClick={() => toggleMenuHandler()}
+          className="h-8 cursor-pointer"
+          src="https://www.svgrepo.com/show/312300/hamburger-menu.svg"
+          alt="Menu"
+        />
+        <a href="/">
+        <img
+          className="h-8 mx-2"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/768px-YouTube_Logo_2017.svg.png"
+          alt="youtube-logo"
+        />
+        </a>
+      </div>
+      <div className="col-span-10 px-10 ">
+        <input
+          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
+          type="text"
+        />
+        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+          🔍
+        </button>
+      </div>
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtRs_rWILOMx5-v3aXwJu7LWUhnPceiKvvDg&s"
+          alt="User-Icon"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
