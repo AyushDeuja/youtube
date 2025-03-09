ba51f1dcabeca781bf454fe263fdbbf1b2949ac8
AyushDeuja <ayushdeuja11@gmail.com>
2025-03-09 20:09:16 +0545

    api

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Button from './Button'
 
 const ButtonList = () => {
+  const list = ["All", "Gaming", "Songs", "Live", "Cricket", "Football", "News", "Cooking", "tmkoc", "Football", "News", "Cooking", "tmkoc"]
   return (
-    <div>
-      
+    <div className='flex'>
+      {list.map((li,index) => <Button key={index} name={li}/>)}
     </div>
   )
 }
