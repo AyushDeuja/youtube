dd5dd84f1ec3d03fdf8280b89f32e5ceb90c4ba0
AyushDeuja <ayushdeuja11@gmail.com>
2025-03-10 09:51:53 +0545

    env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+// const GOOGLE_API_KEY = "AIzaSyC0SD7BQ_7ZEeaQhuQo7YQkuewWMLcV4LE"
 
 export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&maxResults=50&regionCode=US&key=" +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
