1262281d02e2a490d9e3784086156090ce74a738
AyushDeuja <ayushdeuja11@gmail.com>
2025-03-11 20:58:47 +0545

    searchBar created

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -7,18 +7,24 @@ const Head = () => {
   const dispatch = useDispatch();
 
   const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
   useEffect(() => {
     //Api call here
-    console.log(searchQuery);
 
-    getSearchSuggestions();
+    const timer = setTimeout(() => getSearchSuggestions(), 200);
+    return () => {
+      clearTimeout(timer);
+    };
   }, [searchQuery]);
 
   const getSearchSuggestions = async () => {
+    console.log("API CALL = ", searchQuery);
     const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await data.json();
-    console.log(json[1]);
+    // console.log(json[1]);
+    setSuggestions(json[1]);
   };
 
   const toggleMenuHandler = () => {
@@ -26,7 +32,8 @@ const Head = () => {
   };
 
   return (
-    <div className="grid grid-flow-col p-5 m-2 shadow-lg ">
+    <div className="sticky top-0 left-0 right-0 bg-white z-50 grid grid-flow-col p-5 shadow-lg">
+      {/* <div className=" grid grid-flow-col p-5 m-2 shadow-lg "> */}
       <div className="flex col-span-1">
         <img
           onClick={() => toggleMenuHandler()}
@@ -45,15 +52,30 @@ const Head = () => {
         </a>
       </div>
       <div className="col-span-10 px-10 ">
-        <input
-          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
-          type="text"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
-          🔍
-        </button>
+        <div>
+          <input
+            className="px-5 w-1/2 border border-gray-400 p-2 rounded-l-full"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+          />
+          <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+            🔍
+          </button>
+        </div>
+        {showSuggestions && (
+          <div className="fixed bg-white py-2 px-2 w-[32rem] shadow-lg rounded-lg border border-gray-100 ">
+            <ul>
+              {suggestions.map((s) => (
+                <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
+                  🔍 {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
       <div className="col-span-1">
         <img
