ce7fec613e1983d5d4929b98cdbe7e5a36cd8b26
AyushDeuja <ayushdeuja11@gmail.com>
2025-03-17 20:25:30 +0545

    useMemo

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
+import { findPrime } from "../utils/helper";
 
 const Demo = () => {
-  return <div className="m-4 p-2 w-96 h-96 border border-black ">Demo</div>;
+  const [text, setText] = useState("");
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  console.log("Rendering....");
+
+  //   const prime = findPrime(text);
+  const prime = useMemo(() => findPrime(text), [text]);
+
+  return (
+    <div
+      className={
+        "m-4 p-2 w-96 h-96 border border-black " +
+        (isDarkTheme && "bg-gray-900 text-white")
+      }
+    >
+      <button
+        className="bg-green-200 m-10 p-2"
+        onClick={() => {
+          setIsDarkTheme(!isDarkTheme);
+        }}
+      >
+        Toggle
+      </button>
+      <div>
+        <input
+          className="border border-black w-72 px-2"
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
+      <div>
+        <h1 className="mt-4 font-bold text-xl">nth Prime: {prime}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Demo;
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -190,3 +190,23 @@ export function getRandomMessage(length) {
   }
   return randoms.join("");
 }
+
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+export function findPrime(n) {
+  let count = 0;
+  let num = 1;
+  while (count < n) {
+    num++;
+    if (isPrime(num)) {
+      count++;
+    }
+  }
+  return num;
+}
