c56f428ee01a343fa445c9cbdcb9205eca220195
AyushDeuja <ayushdeuja11@gmail.com>
2025-03-18 20:27:21 +0545

    ref completed

--- src/components/Demo2.jsx
+++ src/components/Demo2.jsx
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Demo2 = () => {
   const [y, setY] = useState(0);
   let x = 0;
+
+  const ref = useRef(0);
+  // not like => ref=0
+  // but like ref = {current: 0}
+  const i = useRef(null);
+  useEffect(() => {
+    i.current = setInterval(() => {
+      console.log("Namaste react", Math.random());
+    }, 1000);
+    return () => clearInterval(i.current);
+  }, []);
 
   return (
     <div className="m-4 p-2 bg-slate-50 border border-black w-96 h-96">
@@ -29,6 +40,26 @@ const Demo2 = () => {
         </button>
         <span className="font-bold text-xl">State = {y}</span>
       </div>
+      <div>
+        <button
+          className="bg-green-100 p-2 m-4"
+          onClick={() => {
+            ref.current = ref.current + 1;
+            console.log("ref = ", ref.current);
+          }}
+        >
+          Increase ref
+        </button>
+        <span className="font-bold text-xl">Ref = {ref.current}</span>
+      </div>
+      <button
+        className="bg-red-900 p-4 m-4 text-white font-bold rounded-lg"
+        onClick={() => {
+          clearInterval(i.current);
+        }}
+      >
+        Stop Printing
+      </button>
     </div>
   );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,29 +8,32 @@ const Sidebar = () => {
   if (!isMenuOpen) return null;
   return (
     <div className="p-5 shadow-lg w-48 ">
-        <ul>
-          <li>
-            <Link>Home</Link>
-          </li>
-          <li>Shorts</li>
-          <li>Videos</li>
-          <li>Live</li>
-        </ul>
-        <h1 className="font-bold pt-5">Subscriptions</h1>
-        <ul>
-          <li>Music</li>
-          <li>Sports</li>
-          <li>Gaming</li>
-          <li>Movies</li>
-        </ul>
-        <h1 className="font-bold pt-5">Watch Later</h1>
-        <ul>
-          <li>Music</li>
-          <li>Sports</li>
-          <li>Gaming</li>
-          <li>Movies</li>
-        </ul>
-     </div>
+      <ul>
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/demo"}>Demo</Link>
+        </li>
+        <li>Shorts</li>
+        <li>Videos</li>
+        <li>Live</li>
+      </ul>
+      <h1 className="font-bold pt-5">Subscriptions</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+      <h1 className="font-bold pt-5">Watch Later</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+    </div>
   );
 };
 
